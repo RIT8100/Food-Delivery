e2e50f5ac60c71fe71d890297353c82dddc62415
Ritankar Roy <ritankarroy123@gmail.com>
2026-01-04 20:20:17 +0530

    Creating Admin Dashboard

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
-
+import { Route, Routes } from 'react-router-dom'
+import Add from './pages/Add/Add'
+import List from './pages/List/List'
+import Order from './pages/Orders/Orders'
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <hr />
       <div className="app-content">
-        <Sidebar/>
+        <Sidebar />
+        <Routes>
+          <Route path='/add' element={<Add/>} />
+          <Route path='/list' element={<List/>} />
+          <Route path='/orders' element={<Order/>} />
+        </Routes>
       </div>
     </div>
   )
---
+++ admin/src/pages/Orders/Orders.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Orders.css'
+const Order = () => {
+  return (
+    <div>
+      Orders
+    </div>
+  )
+}
+import './Orders.css'
+export default Order
