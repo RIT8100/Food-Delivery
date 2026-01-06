df310777f34e07a5b93df0ca9e829b8c65e81456
Ritankar Roy <ritankarroy123@gmail.com>
2026-01-06 13:52:02 +0530

    Food add functionality working

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,17 +5,21 @@ import { Route, Routes } from 'react-router-dom'
 import Add from './pages/Add/Add'
 import List from './pages/List/List'
 import Order from './pages/Orders/Orders'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const App = () => {
   return (
     <div>
+      <ToastContainer />
       <Navbar />
       <hr />
       <div className="app-content">
         <Sidebar />
         <Routes>
-          <Route path='/add' element={<Add/>} />
-          <Route path='/list' element={<List/>} />
-          <Route path='/orders' element={<Order/>} />
+          <Route path='/add' element={<Add />} />
+          <Route path='/list' element={<List />} />
+          <Route path='/orders' element={<Order />} />
         </Routes>
       </div>
     </div>
--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,7 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import './Add.css'
+import axios from "axios"
+import { assets } from '../../assets/assets'
+import { toast } from 'react-toastify'
 const Add = () => {
 
+  const url = "http://localhost:4000";
   const [image, setImage] = useState(false)
   const [data, setData] = useState({
     name: "",
@@ -14,14 +18,35 @@ const Add = () => {
     const value = event.target.value;
     setData(data => ({ ...data, [name]: value }))
   }
-  useEffect(() => {
-      console.log(data)
-  }, [data])
+
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault()
+    const formData = new FormData()
+    formData.append("name", data.name)
+    formData.append("description", data.description)
+    formData.append("price", Number(data.price))
+    formData.append("category", data.category)
+    formData.append("image", image)
+    const response = await axios.post(`${url}/api/food/add`, formData);
+    if (response.data.success) {
+      setData({
+        name: "",
+        description: "",
+        price: "",
+        category: "Salad"
+      })
+      setImage(false)
+      toast.success(response.data.message)
+    }
+    else {
+      toast.error(response.data.message)
+    }
+  }
 
   return (
     <div className='add'>
-
-      <form className='flex-col'>
+      <form className='flex-col' onSubmit={onSubmitHandler}>
         <div className="add-image-upload flex-col">
           <p>Upload Image</p>
           <label htmlFor="image">
@@ -61,6 +86,5 @@ const Add = () => {
     </div>
   )
 }
-import './Add.css'
-import { assets } from '../../assets/assets'
+
 export default Add
